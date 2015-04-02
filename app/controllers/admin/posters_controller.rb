9aef035eb5ae62fba30f7fbc87f91e83bdf7777a
Steven Bierwagen <bierwagen.steven@gmail.com>
2015-04-02 03:01:06 -0300

    fixed an issue with uploading a .csv file in windows

--- app/controllers/admin/posters_controller.rb
+++ app/controllers/admin/posters_controller.rb
@@ -9,7 +9,7 @@ class Admin::PostersController < ApplicationController
 		@file = params[:file]
 		if @file.blank?
 			redirect_to admin_posters_path, :notice => "File missing"
-		elsif @file.content_type == "text/csv"
+		elsif File.extname(@file.original_filename) == ".csv"
 			CSV.foreach(@file.path, headers: true, encoding: 'windows-1251:utf-8') do |row|
 				poster = Poster.where(number: row['number'])
 				if poster.count == 1
