52a1793d60c5d73a412d6f4fcf939bc1aa80bbec
Weijiao <weijiaogong@gmail.com>
2016-11-02 17:24:54 +0000

    add posters validations

--- app/models/poster.rb
+++ app/models/poster.rb
@@ -2,15 +2,20 @@ class Poster < ActiveRecord::Base
 	#attr_accessible :number, :presenter, :title, :advisors, :scores_count, :email
 	has_many :scores, dependent: :destroy
 	has_many :judges, through: :scores
-
+	validates :presenter, :email, :title, :advisors, presence: true
+	#validates :number, uniqueness: true # not sure whether the number should be unique
+	
 	def self.import_csv(file)
 		CSV.foreach(file.path, headers: true, encoding: 'windows-1251:utf-8') do |row|
-			poster = Poster.where(number: row['number'])
+			posters = Poster.where(number: row['number'])
 			row_hash = row.to_hash
-			if poster.count == 1
-				poster.first.update_attributes(row_hash)
+			if posters.count == 1
+			   poster = posters.first
+			   poster.assign_attributes(row_hash)
+               poster.save!(validate: false)
 			else
-				Poster.create(row_hash)
+				newposter = Poster.new(row_hash)
+				newposter.save!(validate: false)
 			end
 		end
 	end
