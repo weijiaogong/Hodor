8f78b57b0f0d62af32be07d483ecdf0341883f5d
Muhammad Umair <m.umair10@tamu.edu>
2016-11-06 23:53:41 +0000

    Emails being sent successfully when posters uploaded, now need to add time of event and send emails 24 hours before

--- app/mailers/remind_mailer.rb
+++ app/mailers/remind_mailer.rb
@@ -1,2 +1,9 @@
 class RemindMailer < ApplicationMailer
+    
+    
+    def remind_email(presenter)
+         mail(to: presenter.email , subject: 'Reminder about the IAP Poster Competition')
+    end
+  
+  
 end
