0dea086b64a7e0a0554c8a9f65cb352a4aad4830
Weijiao <weijiaogong@gmail.com>
2016-11-16 00:51:31 +0000

    secret token

--- config/initializers/secret_token.rb
+++
@@ -1,1 +0,0 @@
-PosterJudging::Application.config.secret_key_base = '9417f9a3a5e5af604fde0550cfb55bbdb557cd318264f6ef00e63ed2b590f9f64ec2d9d575e68c2a026f47e85b51ece81fec04f485f3fe12c096f1297709edf4'
