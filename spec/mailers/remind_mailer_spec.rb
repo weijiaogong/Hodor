3dc1ed84dcd2b6a327c12662409add6403e5cec0
Rachel Flores_Meath <rfloresmeath@tamu.edu>
2016-11-22 17:28:30 +0000

    Fixing some rspec

--- spec/mailers/remind_mailer_spec.rb
+++ spec/mailers/remind_mailer_spec.rb
@@ -1,5 +1,5 @@
-require "rails_helper"
+require "spec_helper"
 
-RSpec.describe RemindMailerMailer, type: :mailer do
+RSpec.describe RemindMailer, type: :mailer do
   pending "add some examples to (or delete) #{__FILE__}"
 end
