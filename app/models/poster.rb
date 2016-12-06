8ab5ea5abb7147baf78bce4856b3cf283e86d3de
Rachel Flores_Meath <rfloresmeath@tamu.edu>
2016-12-06 20:20:22 +0000

    Sorting posters and allowing for dupes in poster upload

--- app/models/poster.rb
+++ app/models/poster.rb
@@ -7,38 +7,35 @@ class Poster < ApplicationRecord
 	
 	
 	def self.import(data)
-		if((data.count + Poster.all.count) < Event.find(1).max_poster_number + 1)
-			data.each do |row|
-				row_hash = row.to_hash
-				if row_hash.keys.any? {|k| not(["presenter", "title", "advisors", "email"].include?(k))}
-					return "Invalid column header- valid options are presenter, title, advisors, email"
-				end
-				
-				if not ["presenter", "title", "advisors", "email"].all? {|k| (row_hash.keys.include?(k))}
-					return "Missing column header- presenter, title, advisors, email are required"
-				end
-				
-				#if title or presenter both change, we don't know if we have an old poster, so assume a new one
-				poster = Poster.where("presenter = ? or title = ?", row_hash["presenter"], row_hash["title"]).first	#protip/note to self: :presenter not eq "presenter"
-	
-				if not poster.nil?
-					poster.update_attributes(row_hash)
-				else
-					poster = Poster.create(row_hash.merge({:number => Poster.count + 1}))
-					if not poster.errors.messages.empty?
-						return "Missing fields- please make sure all entries are not blank"
-					end
-				end
-				
-				# Send Confirmation email to all the posters in the csv file uploaded
-				RemindMailer.confirmation_email(row_hash).deliver_later
-				
+		data.each do |row|
+			row_hash = row.to_hash
+			if row_hash.keys.any? {|k| not(["presenter", "title", "advisors", "email"].include?(k))}
+				return "Invalid column header- valid options are presenter, title, advisors, email"
 			end
 			
-			return "Import successful"
-		else
-			return "Error: Exceeding poster limit"
+			if not ["presenter", "title", "advisors", "email"].all? {|k| (row_hash.keys.include?(k))}
+				return "Missing column header- presenter, title, advisors, email are required"
+			end
+			
+			#if title or presenter both change, we don't know if we have an old poster, so assume a new one
+			poster = Poster.where("presenter = ? or title = ?", row_hash["presenter"], row_hash["title"]).first	#protip/note to self: :presenter not eq "presenter"
+
+			if not poster.nil?
+				poster.update_attributes(row_hash)
+			elsif Poster.count < Event.find(1).max_poster_number	#we could have duplicated posters, so we check if we still have room one poster at a time
+				poster = Poster.create(row_hash.merge({:number => Poster.count + 1}))
+				if not poster.errors.messages.empty?
+					return "Missing fields- please make sure all entries are not blank"
+				end
+			else
+				return "Error: Exceeding poster limit"
+			end
+			
+			# Send Confirmation email to all the posters in the csv file uploaded
+			RemindMailer.confirmation_email(row_hash).deliver_later
 		end
+		
+		return "Import successful"
 	end
 
 	def self.find_least_judged()
